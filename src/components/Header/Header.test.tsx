026267cf3e58732831bfddbb77eb6e6c6b486a3d
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-10-01 13:51:37 +0200

    Configure react testing library and create Header component test

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+describe("Given the Header component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show 'Boored' inside a heading", () => {
+      const headerTitleText = "Boored";
+
+      render(<Header />);
+
+      const headerTitle = screen.getByRole("heading", {
+        name: headerTitleText,
+      });
+
+      expect(headerTitle).toBeInTheDocument();
+    });
+  });
+});
