e565143df4cdcbad0d25d4fe0af854a3f476dbb6
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-10-03 10:39:20 +0200

    Create SocialActivitiesPage and ActivityCard components

---
+++ src/activities/components/ActivityCard.tsx
@@ -0,0 +1,22 @@
+import "./ActivityCard.test.css";
+
+const ActivityCard = (): React.ReactElement => {
+  return (
+    <>
+      <img
+        src="/oyster.webp"
+        alt="imatge d'una ostra de dibuixos feliç"
+        width={200}
+        height={200}
+      />
+      <div className="activityCard-container">
+        <h2 className="activityCard-container__title">
+          Troba alguna cosa a fer
+        </h2>
+        <button className="activityCard-container__button">Generar</button>
+      </div>
+    </>
+  );
+};
+
+export default ActivityCard;
