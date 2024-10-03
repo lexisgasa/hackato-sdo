54092630ab28fc96df8900b1cee1e533b7947957
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-10-03 13:03:07 +0200

    Add memory router as a wrapper

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,12 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Given the Header component", () => {
   describe("When it is rendered", () => {
     test("Then it should show 'Boored' inside a heading", () => {
       const headerTitleText = /boored/i;
 
-      render(<Header />);
+      render(
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>
+      );
 
       const headerTitle = screen.getByRole("heading", {
         name: headerTitleText,
