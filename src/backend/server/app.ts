84362b1291299fcc67a000719e80b61268a33532
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-10-05 17:20:57 +0200

    Make the API REST a proxy by adding CORS

--- src/backend/server/app.ts
+++ src/backend/server/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import axios from "axios";
+import cors from "cors";
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors());
 
 const category = "";
 
