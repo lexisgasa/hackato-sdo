a8a1b06cd1c9f571f128867903c43d71d44f14f1
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-10-05 12:15:40 +0200

    Install express and configure TypeScript for backend

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        globals: true,
+        setupFiles: ["./src/setupTests.ts"],
+        environment: "jsdom",
+        coverage: {
+            include: ["src/**/*.tsx"],
+            exclude: ["src/main.tsx"],
+        },
+    },
+});
